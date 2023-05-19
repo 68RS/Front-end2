5309ab3ae0a1ce6b2c27f52c143dd6b1ebc991b5
RobsonSantos <rboxe68@gmail.com>
2023-05-19 01:41:14 -0300

    atualização

--- Aula5_mesa_de_trabalho/mesa.js
+++ Aula5_mesa_de_trabalho/mesa.js
@@ -1,11 +1,11 @@
-const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+const darkRef = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
 }
 
-if (prefersDarkMode) {
+if (darkRef) {
     toggleDarkMode();
 }
 
